3bd9baf5c3aa2efa1260d530a101d6705e01e03e
Ethan Franz <efranz@ualberta.ca>
2022-09-29 18:15:43 -0600

    Added the Pentagon CLass git

--- app/src/main/java/com/example/cmput301f22t15/Pentagon.java
+++ app/src/main/java/com/example/cmput301f22t15/Pentagon.java
@@ -1,2 +1,6 @@
-package com.example.cmput301f22t15;public class Pentagon {
+package com.example.cmput301f22t15;
+
+public class Pentagon extends Shape{
+    private int sides = 5;
+    private double sideLength;
 }
